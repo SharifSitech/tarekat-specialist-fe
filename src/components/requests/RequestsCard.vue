65af604d1b517b54e9129e995be65f06297771d5
SharifSitech <128479545+SharifSitech@users.noreply.github.com>
2024-04-23 21:58:57 +0300

    add request card

---
+++ src/components/requests/RequestsCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  number: [String, Number]
+});
+</script>
+
+<template>
+  <div class="flex me-3 mt-2 items-center gap-6 rounded-[8px] bg-[#FFF] p-[13px] w-[236px]">
+    <div class="flex w-full">
+      <slot name="icon-with-title"></slot>
+    </div>
+
+    <p class="text-sm text-[#1F2120] self-end">{{ props.number }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/requests/RequestsView.vue
+++ src/views/requests/RequestsView.vue
@@ -1,13 +1,64 @@
-<script setup lang="ts">
+<script setup>
+import {ref} from "vue";
+import FileUpIcon from "@/components/icons/FileUpIcon.vue";
+import RequestsCard from "@/components/requests/RequestsCard.vue";
+import FileEyeIcon from "@/components/icons/FileEyeIcon.vue";
+import FileCheckIcon from "@/components/icons/FileCheckIcon.vue";
+import FileXIcon from "@/components/icons/FileXIcon.vue";
 
+const selectedCity = ref();
+const cities = ref([
+  {name: 'New York', code: 'NY'},
+  {name: 'Rome', code: 'RM'},
+  {name: 'London', code: 'LDN'},
+  {name: 'Istanbul', code: 'IST'},
+  {name: 'Paris', code: 'PRS'}
+]);
 </script>
 
 <template>
-  <div>
-    Requests List
+  <div class="requests">
+    <p class="flex text-[22px] text-[#A0A3A2] leading-[130%] font-bold">الطلبات</p>
+    <div class="flex">
+      <RequestsCard class="justify-between" number="3">
+        <template v-slot:icon-with-title>
+          <FileUpIcon class="me-1"/>
+          <p class="text-sm text-[#1F2120]">طلبات جديدة</p>
+        </template>
+      </RequestsCard>
+
+      <RequestsCard class="justify-between" number="6">
+        <template v-slot:icon-with-title>
+          <FileEyeIcon class="me-1"/>
+          <p class="text-sm text-[#1F2120]">طلبات قيد التنفيذ</p>
+        </template>
+      </RequestsCard>
+
+      <RequestsCard class="justify-between" number="10">
+        <template v-slot:icon-with-title>
+          <FileCheckIcon class="me-1"/>
+          <p class="text-sm text-[#1F2120]">طلبات منتهية</p>
+        </template>
+      </RequestsCard>
+
+      <RequestsCard class="justify-between" number="4">
+        <template v-slot:icon-with-title>
+          <FileXIcon class="me-1"/>
+          <p class="text-sm text-[#1F2120]">طلبات ملغاة</p>
+        </template>
+      </RequestsCard>
+      <Button label="Submit" />
+      <!--            <Dropdown-->
+      <!--                v-model="selectedCity"-->
+      <!--                :options="[]"-->
+      <!--                optionLabel="name"-->
+      <!--                placeholder="الاجراءات"-->
+      <!--                class="w-full"/>-->
+    </div>
   </div>
 </template>
 
 <style scoped>
+
 
 </style>
